70b23d0e7fb24d393e6d35e72da5747c65d1b14e
Danick <45609733+RestartFU@users.noreply.github.com>
2022-01-09 19:40:14 -0500

    Update handler.go

--- handler.go
+++ handler.go
@@ -1,6 +1,8 @@
 package items
 
 import (
+	"fmt"
+
 	"github.com/df-mc/dragonfly/server/event"
 	"github.com/df-mc/dragonfly/server/player"
 )
@@ -21,10 +23,14 @@ func NewPlayerHandler(p *player.Player) *PlayerHandler { return &PlayerHandler{p
 func (h *PlayerHandler) HandleItemUse(*event.Context) {
 	p := h.p                     // Handled player
 	heldItem, _ := p.HeldItems() // Main hand
+	fmt.Println("Held Item: ", heldItem.CustomName())
 
 	if i, ok := Compatible(heldItem); ok { //Making sure it is registered
+		fmt.Println("Item Is Compatible")
 		if usable, ok := i.(UsableItem); ok { // Hopefully usable
+			fmt.Println("Item Is Usable")
 			usable.Use(heldItem, p)
+			fmt.Println("Used Item")
 		}
 	}
 }
